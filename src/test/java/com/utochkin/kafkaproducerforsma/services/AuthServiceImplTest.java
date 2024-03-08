660d283da0d4f2d0332fc4913485b043cf90e5e4
Sergei Utochkin <dzaga73i98@gmail.com>
2024-03-08 10:09:10 +0300

    refactoring AuthServiceImplTest

--- src/test/java/com/utochkin/kafkaproducerforsma/services/AuthServiceImplTest.java
+++ src/test/java/com/utochkin/kafkaproducerforsma/services/AuthServiceImplTest.java
@@ -2,6 +2,8 @@ package com.utochkin.kafkaproducerforsma.services;
 
 import com.utochkin.kafkaproducerforsma.dto.request.JwtRequest;
 import com.utochkin.kafkaproducerforsma.dto.response.JwtResponse;
+import com.utochkin.kafkaproducerforsma.exceptions.AccessDeniedException;
+import com.utochkin.kafkaproducerforsma.exceptions.BadCredentialsException;
 import com.utochkin.kafkaproducerforsma.models.Role;
 import com.utochkin.kafkaproducerforsma.models.User;
 import com.utochkin.kafkaproducerforsma.security.JwtTokenProvider;
@@ -20,8 +22,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceImplTest {
@@ -65,11 +66,33 @@ class AuthServiceImplTest {
 
     @Test
     void login() {
+        doReturn(user1).when(userService).findByName(user1.getName());
         doReturn("TOKEN_USER1").when(jwtTokenProvider).createAccessToken(user1.getId(), user1.getName(), user1.getRole());
         doReturn("REFRESH_TOKEN_USER1").when(jwtTokenProvider).createRefreshToken(user1.getId(), user1.getName());
-        doReturn(user1).when(userService).findByName("Sergey");
         Assertions.assertEquals(jwtResponse, authService.login(jwtRequest));
         verify(authenticationManager, Mockito.times(1)).authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    @Test
+    void incorrectNameLogin() {
+        JwtRequest incorrectJwtRequestName = JwtRequest.builder()
+                .name("S")
+                .password("111")
+                .build();
+        usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(incorrectJwtRequestName.getName(), incorrectJwtRequestName.getPassword());
+        doThrow(BadCredentialsException.class).when(authenticationManager).authenticate(usernamePasswordAuthenticationToken);
+        Assertions.assertThrows(BadCredentialsException.class, () -> authService.login(incorrectJwtRequestName));
+    }
+
+    @Test
+    void incorrectNamePassword() {
+        JwtRequest incorrectJwtRequestPassword = JwtRequest.builder()
+                .name("Sergey")
+                .password("1")
+                .build();
+        usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(incorrectJwtRequestPassword.getName(), incorrectJwtRequestPassword.getPassword());
+        doThrow(BadCredentialsException.class).when(authenticationManager).authenticate(usernamePasswordAuthenticationToken);
+        Assertions.assertThrows(BadCredentialsException.class, () -> authService.login(incorrectJwtRequestPassword));
     }
 
     @Test
@@ -77,6 +100,12 @@ class AuthServiceImplTest {
         JwtResponse refreshJwtResponse = new JwtResponse(user1.getId(), user1.getName(), "TOKEN_USER1", "NEW_REFRESH_TOKEN_USER1");
         doReturn(refreshJwtResponse).when(jwtTokenProvider).refreshUserTokens("REFRESH_TOKEN_USER1");
         Assertions.assertEquals(refreshJwtResponse, authService.refresh("REFRESH_TOKEN_USER1"));
+    }
+
+    @Test
+    void refreshAccessDenied() {
+        doThrow(AccessDeniedException.class).when(jwtTokenProvider).refreshUserTokens(anyString());
+        Assertions.assertThrows(AccessDeniedException.class, () -> authService.refresh(anyString()));
     }
 
 }
